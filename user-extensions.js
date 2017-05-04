b534523f048de602189103b635a1ba07ac0f4911
Rodrigo Manrique <rodrigo.manrique@globant.com>
2017-05-04 17:10:12 -0300

    Ejercicio3

--- user-extensions.js
+++ user-extensions.js
@@ -17,3 +17,27 @@ Selenium.prototype.assertFormMenu = function(locator, value) {
         Assert.matches(args[j], arr[j]);
     }
 };
+
+
+Selenium.prototype.doStoreRandomEmail = function(varName) {
+    var strValues = "abcdefghiklmnopqrstuvwxyz0123456789";
+    var stringLength = 8;
+    var strEmail = "";
+    var strTmp;
+
+    for (var i = 0; i < stringLength; i++) {
+        strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
+        strEmail = strEmail + strTmp;
+    }
+    strTmp = "";
+    strEmail = strEmail + "@";
+
+    for (var j = 0; j < 5; j++) {
+        strTmp = strValues.charAt(Math.round(strValues.length * Math.random()));
+        strEmail = strEmail + strTmp;
+    }
+    strEmail = strEmail + ".com"
+
+    storedVars[varName] = strEmail;
+
+};
